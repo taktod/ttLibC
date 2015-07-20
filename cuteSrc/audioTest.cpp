eaf817c5b32d3543b9f3f6c830189f5f173b2298
taktod <poepoemix@hotmail.com>
2015-07-20 20:42:21 +0900

    to make mp3lame encoder. first of all, write a encode code.

--- cuteSrc/audioTest.cpp
+++ cuteSrc/audioTest.cpp
@@ -9,6 +9,61 @@
  */
 
 #include <cute.h>
+#include <ttLibC/log.h>
+
+#include <ttLibC/util/beepUtil.h>
+#include <ttLibC/util/hexUtil.h>
+
+#ifdef __ENABLE_MP3LAME__
+#	include <lame/lame.h>
+#endif
+
+static void mp3LameTest() {
+	LOG_PRINT("mp3LameTest");
+#ifdef __ENABLE_MP3LAME_ENCODE__
+	LOG_PRINT("version:%s", get_lame_version());
+
+	lame_global_flags *gflags = NULL;
+	gflags = lame_init();
+
+	if(gflags == NULL) {
+		ERR_PRINT("failed to initialize lame.");
+		return;
+	}
+
+	lame_set_num_channels(gflags, 1);
+	lame_set_mode(gflags, MONO);
+	lame_set_in_samplerate(gflags, 22050);
+	lame_set_out_samplerate(gflags, 22050);
+
+	lame_set_quality(gflags, 5);
+	lame_set_VBR(gflags, vbr_default);
+	lame_set_VBR_quality(gflags, 5);
+	lame_set_bWriteVbrTag(gflags, 0);
+
+	if(lame_init_params(gflags) < 0) {
+		ERR_PRINT("failed to setup lame.");
+		return;
+	}
+	FILE *fp = fopen("output.mp3", "wb");
+	ttLibC_BeepGenerator *generator = ttLibC_BeepGenerator_make(PcmS16Type_littleEndian_planar, 440, 22050, 1);
+	ttLibC_PcmS16 *pcm = NULL, *p;
+	for(int i = 0;i < 10;i ++) {
+		p = ttLibC_BeepGenerator_makeBeepByMiliSec(generator, pcm, 1000);
+		if(p == NULL) {
+			break;
+		}
+		pcm = p;
+		uint8_t buf[65536];
+		uint32_t size = lame_encode_buffer(gflags, (const short *)pcm->inherit_super.inherit_super.data, NULL, pcm->inherit_super.sample_num, buf, sizeof(buf));
+		fwrite(buf, size, 1, fp);
+		ttLibC_HexUtil_dump(buf, size, true);
+	}
+	ttLibC_BeepGenerator_close(&generator);
+	fclose(fp);
+	lame_close(gflags);
+#endif
+}
 
 /**
  * define all test for audio package.
@@ -17,5 +72,6 @@
  */
 cute::suite audioTests(cute::suite s) {
 	s.clear();
+	s.push_back(CUTE(mp3LameTest));
 	return s;
 }
