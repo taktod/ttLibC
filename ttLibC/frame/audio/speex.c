4f4211aa20002cd8796e5c4cde02d5b63fab981a
taktod <poepoemix@hotmail.com>
2016-08-20 23:17:35 +0900

    add getFrame function for speex frame.

--- ttLibC/frame/audio/speex.c
+++ ttLibC/frame/audio/speex.c
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 #include "../../log.h"
 #include "../../allocator.h"
+#include "../../util/ioUtil.h"
+#include "../../util/byteUtil.h"
 
 typedef ttLibC_Frame_Audio_Speex ttLibC_Speex_;
 
@@ -102,6 +104,94 @@ ttLibC_Speex *ttLibC_Speex_make(
 	return (ttLibC_Speex *)speex;
 }
 
+static int narrowSize[] = {5, 43, 119, 160, 220, 300, 364, 492, 79};
+static int wideSize[] = {4, 36, 112, 192, 352};
+typedef enum SpeexBand {
+	unknown = 0x00,
+	narrow = 0x01,
+	wide = 0x02,
+	super_wide = 0x03
+} SpeexBand;
+
+/**
+ * analyze speex band frames.
+ * @param reader    target byte reader.
+ * @param data_size total applyed data size.
+ * @param frame_num result of contain frame #.
+ * @param bandtype  result of contain speex band type.
+ * @return true:ok false:error
+ * should I return Error_t?
+ */
+static bool Speex_analyzeFrameBuffer(
+		ttLibC_ByteReader *reader,
+		size_t data_size,
+		uint32_t *frame_num,
+		SpeexBand *bandtype) {
+	uint32_t readingBitSize = 0;
+	*frame_num = 0;
+	*bandtype = unknown;
+	do {
+		// check first bit to understand narrow or wide.
+		if(ttLibC_ByteReader_bit(reader, 1) == 0) {
+			++ *frame_num;
+			if(*frame_num == 1) {
+				++ *bandtype;
+			}
+			// next 4bit for type.
+			uint32_t type = ttLibC_ByteReader_bit(reader, 4);
+			if(type > 8) {
+				*frame_num = 0;
+				*bandtype = unknown;
+				return false;
+			}
+			// update bit size to skip.
+			readingBitSize = narrowSize[type] - 5;
+			while(readingBitSize > 0) {
+				if(readingBitSize > 32) {
+					ttLibC_ByteReader_bit(reader, 32);
+					readingBitSize -= 32;
+				}
+				else {
+					ttLibC_ByteReader_bit(reader, readingBitSize);
+					readingBitSize = 0;
+				}
+			}
+		}
+		else {
+			if(*frame_num == 1) {
+				++ *bandtype;
+			}
+			// next 3bit for type.
+			uint32_t type = ttLibC_ByteReader_bit(reader, 3);
+			if(type > 4) {
+				*frame_num = 0;
+				*bandtype = unknown;
+				return false;
+			}
+			// update bit size to skip.
+			readingBitSize = wideSize[type] - 4;
+			while(readingBitSize > 0) {
+				if(readingBitSize > 32) {
+					ttLibC_ByteReader_bit(reader, 32);
+					readingBitSize -= 32;
+				}
+				else {
+					ttLibC_ByteReader_bit(reader, readingBitSize);
+					readingBitSize = 0;
+				}
+			}
+		}
+		if(reader->error != Error_noError) {
+			// in the case of error.
+			*frame_num = 0;
+			*bandtype = unknown;
+			return false;
+		}
+	} while(reader->read_size < data_size - 1); // speex frame can be end without full byte.
+	// ok
+	return true;
+}
+
 /*
  * make speex frame from byte data.
  * @param prev_frame    reuse speex frame.
@@ -119,8 +209,121 @@ ttLibC_Speex *ttLibC_Speex_getFrame(
 		bool non_copy_mode,
 		uint64_t pts,
 		uint32_t timebase) {
-	// TODO make this.
-	return NULL;
+	if(prev_frame == NULL) {
+		uint8_t *buf = data;
+		// first frame can be speex header.
+		if(buf[0] == 'S'
+		&& buf[1] == 'p'
+		&& buf[2] == 'e'
+		&& buf[3] == 'e'
+		&& buf[4] == 'x'
+		&& buf[5] == ' '
+		&& buf[6] == ' '
+		&& buf[7] == ' ') {
+			buf += 0x24;
+			uint32_t *buf_int = (uint32_t *)buf;
+			uint32_t sample_rate = le_uint32_t(*buf_int);
+			buf_int += 3;
+			uint32_t channel_num = le_uint32_t(*buf_int);
+			buf_int += 2;
+			uint32_t sample_num = le_uint32_t(*buf_int);
+			buf_int += 2;
+			sample_num *= le_uint32_t(*buf_int);
+			return ttLibC_Speex_make(
+					NULL,
+					SpeexType_header,
+					sample_rate,
+					sample_num,
+					channel_num,
+					data,
+					data_size,
+					true,
+					pts,
+					timebase);
+		}
+		else {
+			// if not header, try to use as frame data.
+			ttLibC_ByteReader *reader = ttLibC_ByteReader_make(data, data_size, ByteUtilType_default);;
+			uint32_t frame_num;
+			SpeexBand band;
+			uint32_t sample_rate = 0;
+			uint32_t sample_num = 0;
+			if(!Speex_analyzeFrameBuffer(reader, data_size, &frame_num, &band)) {
+				ERR_PRINT("1st speex frame is unknown.");
+			}
+			else {
+				switch(band) {
+				case narrow:
+					sample_rate = 8000;
+					break;
+				case wide:
+					sample_rate = 16000;
+					break;
+				case super_wide:
+					sample_rate = 32000;
+					break;
+				default:
+					// something wrong.
+					break;
+				}
+				sample_num = sample_rate /50 * frame_num;
+			}
+			ttLibC_ByteReader_close(&reader);
+			// if sample_rate is not updated, frame is unknown.
+			if(sample_rate == 0) {
+				return NULL;
+			}
+			else {
+				return ttLibC_Speex_make(
+						NULL,
+						SpeexType_frame,
+						sample_rate,
+						sample_num,
+						1,
+						data,
+						data_size,
+						true,
+						pts,
+						timebase);
+			}
+		}
+	}
+	else {
+		if(prev_frame->type == SpeexType_header) {
+			// in the case of 1st frame is header. next frame can be comment.
+			// check the data is frame or not.
+			ttLibC_ByteReader *reader = ttLibC_ByteReader_make(data, data_size, ByteUtilType_default);;
+			uint32_t frame_num;
+			SpeexBand band;
+			Speex_analyzeFrameBuffer(reader, data_size, &frame_num, &band);
+			ttLibC_ByteReader_close(&reader);
+			if(band == unknown) {
+				// if it's not frame, treat as comment.
+				return ttLibC_Speex_make(
+						prev_frame,
+						SpeexType_comment,
+						prev_frame->inherit_super.sample_rate,
+						prev_frame->inherit_super.sample_num,
+						prev_frame->inherit_super.channel_num,
+						data,
+						data_size,
+						true,
+						pts,
+						timebase);
+			}
+		}
+		return ttLibC_Speex_make(
+				prev_frame,
+				SpeexType_frame,
+				prev_frame->inherit_super.sample_rate,
+				prev_frame->inherit_super.sample_num,
+				prev_frame->inherit_super.channel_num,
+				data,
+				data_size,
+				true,
+				pts,
+				timebase);
+	}
 }
 
 /*
