30905a9bdcae870d520224c6cff1c88894f307d4
taktod <poepoemix@hotmail.com>
2016-08-20 23:24:05 +0900

    deal with header and comment frames.

--- ttLibC/decoder/speexDecoder.c
+++ ttLibC/decoder/speexDecoder.c
@@ -107,34 +107,50 @@ bool ttLibC_SpeexDecoder_decode(
 		return true;
 	}
 	ttLibC_SpeexDecoder_ *decoder_ = (ttLibC_SpeexDecoder_ *)decoder;
-	speex_bits_read_from(&decoder_->bits, (char *)speex->inherit_super.inherit_super.data, speex->inherit_super.inherit_super.buffer_size);
-	int result = speex_decode_int(decoder_->dec_state, &decoder_->bits, decoder_->pcm_buffer);
-	if(result != 0) {
-		ERR_PRINT("failed to decode.%d", result);
-		return false;
-	}
-	uint32_t sample_num = decoder_->inherit_super.sample_rate / 50;
-	ttLibC_PcmS16 *p = ttLibC_PcmS16_make(
-			decoder_->pcms16,
-			PcmS16Type_littleEndian,
-			decoder_->inherit_super.sample_rate,
-			sample_num,
-			decoder_->inherit_super.channel_num,
-			decoder_->pcm_buffer,
-			decoder_->pcm_buffer_size,
-			decoder_->pcm_buffer,
-			sample_num * 2 * decoder_->inherit_super.channel_num,
-			NULL,
-			0,
-			true,
-			speex->inherit_super.inherit_super.pts,
-			speex->inherit_super.inherit_super.timebase);
-	if(p == NULL) {
-		return false;
-	}
-	decoder_->pcms16 = p;
-	if(!callback(ptr, decoder_->pcms16)) {
-		return false;
+	switch(speex->type) {
+	default:
+	case SpeexType_comment:
+		break;
+	case SpeexType_header:
+		{
+			// TODO need to use these information?
+		}
+		break;
+	case SpeexType_frame:
+		{
+			speex_bits_read_from(&decoder_->bits, (char *)speex->inherit_super.inherit_super.data, speex->inherit_super.inherit_super.buffer_size);
+			int result = speex_decode_int(decoder_->dec_state, &decoder_->bits, decoder_->pcm_buffer);
+			if(result != 0) {
+				ERR_PRINT("failed to decode.%d", result);
+				return false;
+			}
+			uint32_t sample_num = decoder_->inherit_super.sample_rate / 50;
+			ttLibC_PcmS16 *p = ttLibC_PcmS16_make(
+					decoder_->pcms16,
+					PcmS16Type_littleEndian,
+					decoder_->inherit_super.sample_rate,
+					sample_num,
+					decoder_->inherit_super.channel_num,
+					decoder_->pcm_buffer,
+					decoder_->pcm_buffer_size,
+					decoder_->pcm_buffer,
+					decoder_->pcm_buffer_size,
+					NULL,
+					0,
+					true,
+					speex->inherit_super.inherit_super.pts,
+					speex->inherit_super.inherit_super.timebase);
+			if(p == NULL) {
+				return false;
+			}
+			decoder_->pcms16 = p;
+			if(callback != NULL) {
+				if(!callback(ptr, decoder_->pcms16)) {
+					return false;
+				}
+			}
+		}
+		break;
 	}
 	return true;
 }
