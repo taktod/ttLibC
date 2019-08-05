f4063c7755b6f0d2132b4b1d4ee2b151083f6316
taktod <poepoemix@hotmail.com>
2019-08-05 14:33:16 +0900

    update test.

--- gtest/frame.cpp
+++ gtest/frame.cpp
@@ -1,10 +1,18 @@
-#include "gtest/gtest.h"
+#include "test.hpp"
 #include <ttLibC/frame/frame.h>
+#include <ttLibC/frame/video/bgr.h>
 
-TEST(frame, hoge1) {
-  EXPECT_EQ(1,1);
+class FrameTest : public TTTest{
+};
+
+TEST_F(FrameTest, bgr) {
+  ttLibC_Bgr *bgr = ttLibC_Bgr_make(NULL, BgrType_abgr, 320, 240, 1280, NULL, 0, true, 0, 1000);
+  EXPECT_TRUE(bgr != NULL);
+  ttLibC_Bgr_close(&bgr);
 }
 
-TEST(frame, hoge2) {
-  EXPECT_EQ(1,1);
+TEST(frame, bgrMakeEmpty) {
+  ttLibC_Bgr *bgr = ttLibC_Bgr_makeEmptyFrame(BgrType_bgr, 320, 240);
+  EXPECT_TRUE(bgr != NULL);
+  ttLibC_Bgr_close(&bgr);
 }
