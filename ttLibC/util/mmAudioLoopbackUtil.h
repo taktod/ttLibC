97159bd3a83bb033a0de9ae22c7102d09f7b8b60
taktod <poepoemix@hotmail.com>
2019-08-13 12:16:46 +0900

    update

--- ttLibC/util/mmAudioLoopbackUtil.h
+++ ttLibC/util/mmAudioLoopbackUtil.h
@@ -33,17 +33,17 @@ typedef bool (*ttLibC_MmAudioLoopbackDeviceNameFunc)(void *ptr, const char *name
 typedef bool (*ttLibC_MmAudioLoopbackFrameFunc)(void *ptr, ttLibC_PcmS16 *pcm);
 
 bool TT_ATTRIBUTE_API ttLibC_MmAudioLoopback_getDeviceNames(
-	ttLibC_MmAudioLoopbackDeviceNameFunc callback,
-	void *ptr);
+  ttLibC_MmAudioLoopbackDeviceNameFunc callback,
+  void *ptr);
 
 ttLibC_MmAudioLoopback TT_ATTRIBUTE_API *ttLibC_MmAudioLoopback_make(
-	const char *locale,
-	const char *deviceName);
+  const char *locale,
+  const char *deviceName);
 
 bool TT_ATTRIBUTE_API ttLibC_MmAudioLoopback_queryFrame(
-	ttLibC_MmAudioLoopback *device,
-	ttLibC_MmAudioLoopbackFrameFunc callback,
-	void *ptr);
+  ttLibC_MmAudioLoopback *device,
+  ttLibC_MmAudioLoopbackFrameFunc callback,
+  void *ptr);
 
 void TT_ATTRIBUTE_API ttLibC_MmAudioLoopback_close(ttLibC_MmAudioLoopback **device);
 
