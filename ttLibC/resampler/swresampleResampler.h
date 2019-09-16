d98c4d25fb571cf8699e3ad19fee9b4751c6178f
taktod <poepoemix@hotmail.com>
2019-09-16 12:44:39 +0900

    from tab to space.

--- ttLibC/resampler/swresampleResampler.h
+++ ttLibC/resampler/swresampleResampler.h
@@ -18,10 +18,10 @@ extern "C" {
 #include "../frame/frame.h"
 
 typedef struct ttLibC_Resampler_SwresampleResampler {
-	/** target sample_rate */
-	uint32_t sample_rate;
-	/** target channel_num */
-	uint32_t channel_num;
+  /** target sample_rate */
+  uint32_t sample_rate;
+  /** target channel_num */
+  uint32_t channel_num;
 } ttLibC_Resampler_SwresampleResampler;
 
 typedef ttLibC_Resampler_SwresampleResampler ttLibC_SwresampleResampler;
@@ -29,20 +29,20 @@ typedef ttLibC_Resampler_SwresampleResampler ttLibC_SwresampleResampler;
 typedef bool (* ttLibC_getSwresampleFrameFunc)(void *ptr, ttLibC_Frame *frame);
 
 ttLibC_SwresampleResampler TT_ATTRIBUTE_API *ttLibC_SwresampleResampler_make(
-		ttLibC_Frame_Type input_frame_type,
-		uint32_t          input_sub_type,
-		uint32_t          input_sample_rate,
-		uint32_t          input_channel_num,
-		ttLibC_Frame_Type output_frame_type,
-		uint32_t          output_sub_type,
-		uint32_t          output_sample_rate,
-		uint32_t          output_channel_num);
+    ttLibC_Frame_Type input_frame_type,
+    uint32_t          input_sub_type,
+    uint32_t          input_sample_rate,
+    uint32_t          input_channel_num,
+    ttLibC_Frame_Type output_frame_type,
+    uint32_t          output_sub_type,
+    uint32_t          output_sample_rate,
+    uint32_t          output_channel_num);
 
 bool TT_ATTRIBUTE_API ttLibC_SwresampleResampler_resample(
-		ttLibC_SwresampleResampler *resampler,
-		ttLibC_Frame *frame,
-		ttLibC_getSwresampleFrameFunc callback,
-		void *ptr);
+    ttLibC_SwresampleResampler *resampler,
+    ttLibC_Frame *frame,
+    ttLibC_getSwresampleFrameFunc callback,
+    void *ptr);
 
 void TT_ATTRIBUTE_API ttLibC_SwresampleResampler_close(ttLibC_SwresampleResampler **resampler);
 
