21d898f68d0fe3406a164d694ad4d6bb9b71b36c
taktod <poepoemix@hotmail.com>
2020-11-09 15:38:08 +0900

    this should be better.

--- ttLibCpp/container/mkv.cpp
+++ ttLibCpp/container/mkv.cpp
@@ -680,7 +680,9 @@ MkvWriter *MkvWriter::create(vector<ttLibC_Frame_Type> types) {
   return new MkvWriter_(types);
 }
 MkvWriter *MkvWriter::create(ttLibC_Frame_Type videoType, ttLibC_Frame_Type audioType) {
-  std::vector<ttLibC_Frame_Type> vec(videoType, audioType);
+  std::vector<ttLibC_Frame_Type> vec;
+  vec.push_back(videoType);
+  vec.push_back(audioType);
   return new MkvWriter_(vec);
 }
 
